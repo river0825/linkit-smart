3e2702f2e837b427ccc276f84582fec42a4ea764
river0825 <riverdm@gmail.com>
2017-09-13 11:45:04 +0800

    Update mount-root-fs-on-sd.sh

--- mount-root-fs-on-sd.sh
+++ mount-root-fs-on-sd.sh
@@ -6,13 +6,14 @@ if df -h | grep -q  "mcblk0p1.*/overlay$"; then
         exit 0
 fi
 
-
-read -p "Following process will ERASE all data in you sdcard. Do you want to proceed? (Y/N)" yn
-case $yn in
-        [Yy]* ) break ;;
-        [Nn]* ) echo "program exit"; exit;;
-        * ) echo "Please answer Y/N";;
-esac
+while true; do
+	read -p "Following process will ERASE all data in you sdcard. Do you want to proceed? (Y/N)" yn
+	case $yn in
+		[Yy]* ) break ;;
+		[Nn]* ) echo "program exit"; exit;;
+		* ) echo "Please answer Y/N";;
+	esac
+done
 
 echo "========= updating opkg ========="
 opkg update
@@ -36,12 +37,12 @@ echo "========= Open the fstab configuration and edit it ========="
 sed -i -- 's/mnt\/mcblk0p1/overlay/g' *
 sed -i -- 's/enabled '0'/enabled '1'/g' *
 
-
-read -p "All setting have been done, do you want to reboot? (Y/N)" yn
-case $yn in
-	[Yy]* ) reboot; break ;;
-	[Nn]* ) echo "you have to reboot to make it work"; exit;;
-	* ) echo "Please answer Y/N";;
-esac
-
+while true; do
+	read -p "All setting have been done, do you want to reboot? (Y/N)" yn
+	case $yn in
+		[Yy]* ) reboot; break ;;
+		[Nn]* ) echo "========= you have to reboot to make it work ========="; exit;;
+		* ) echo "Please answer Y/N";;
+	esac
+done
  
