185f80cd683eff262f233cc1df422caf185d0078
river0825 <riverdm@gmail.com>
2017-09-13 03:20:22 +0000

    check if root fs has been modified

--- mount-root-fs-on-sd.sh
+++ mount-root-fs-on-sd.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 
+#check if sd card has been mount as root
+if df -h | grep -q  "mcblk0p1.*/overlay$"; then
+        echo "sd card has been mount as root, exit"
+        exit 0
+fi
+
+
+read -p "Following process will erase all data in you SDCard, do you want to proceed? (Y/N)" yn
+case $yn in
+        [Yy]* ) break ;;
+        [Nn]* ) echo "program exit";;
+                exit;;
+        * ) echo "Please answer Y/N";;
+esac
+
+echo "updating opkg"
 opkg update
 opkg install block-mount kmod-fs-ext4 kmod-usb-storage-extras e2fsprogs fdisk
 
-#Format the SD card. ext4 file system is used in this example:
+echo "Format the SD card. ext4 file system"
 mkfs.ext4 /dev/mmcblk0p1
 
-#Duplicate the current root FS and move it to the SD card by typing the following commands:
+echo "Duplicate the current root FS and move it to the SD card"
 mount /dev/mmcblk0p1 /mnt
 tar -C /overlay -cvf - . | tar -C /mnt -xf -
 umount /mnt
 
-#Create a fstab template, for example:
+echo "Create a fstab template"
 block detect > /etc/config/fstab
 
-#Open the fstab configuration and edit it
+echo "Open the fstab configuration and edit it"
 sed -i -- 's/mnt\/mcblk0p1/overlay/g' *
 sed -i -- 's/enabled '0'/enabled '1'/g' *
+
+
+read -p "All setting have been done, do you want to reboot? (Y/N)" yn
+case $yn in
+	[Yy]* ) reboot; break ;;
+	[Nn]* ) echo "you have to reboot to make it work";;
+		exit;;
+	* ) echo "Please answer Y/N";;
+esac
+
+ 
