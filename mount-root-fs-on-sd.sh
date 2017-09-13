01a64639c575f9b0b39a065029ba626bfa3208e2
river0825 <riverdm@gmail.com>
2017-09-13 11:40:11 +0800

    Update mount-root-fs-on-sd.sh

--- mount-root-fs-on-sd.sh
+++ mount-root-fs-on-sd.sh
@@ -7,32 +7,32 @@ if df -h | grep -q  "mcblk0p1.*/overlay$"; then
 fi
 
 
-read -p "Following process will erase all data in you SDCard, do you want to proceed? (Y/N)" yn
+read -p "Following process will ERASE all data in you sdcard. Do you want to proceed? (Y/N)" yn
 case $yn in
         [Yy]* ) break ;;
         [Nn]* ) echo "program exit"; exit;;
         * ) echo "Please answer Y/N";;
 esac
 
-echo "updating opkg"
+echo "========= updating opkg ========="
 opkg update
 opkg install block-mount kmod-fs-ext4 kmod-usb-storage-extras e2fsprogs fdisk
 
-echo "Format the SD card. ext4 file system"
+echo "========= Format the SD card. ext4 file system ========="
 mkfs.ext4 /dev/mmcblk0p1
 
-echo "Duplicate the current root FS and move it to the SD card"
-if mount /dev/mmcblk0p1 /mnt; then
+echo "========= Duplicate the current root FS and move it to the sdcard ========="
+if ! mount /dev/mmcblk0p1 /mnt; then
 	echo "sdcard mount fail, try again with another sdcard"
 	exit
 fi
 tar -C /overlay -cvf - . | tar -C /mnt -xf -
 umount /mnt
 
-echo "Create a fstab template"
+echo "========= Create a fstab template ========="
 block detect > /etc/config/fstab
 
-echo "Open the fstab configuration and edit it"
+echo "========= Open the fstab configuration and edit it ========="
 sed -i -- 's/mnt\/mcblk0p1/overlay/g' *
 sed -i -- 's/enabled '0'/enabled '1'/g' *
 
